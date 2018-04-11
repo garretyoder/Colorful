c521869bcdc540b40eecdd55e2275c624b773e80
MFlisar <mflisar.development@gmail.com>
2018-04-11 19:15:13 +0200

    code cleaned + added a basic common interface for AppCompatActivity and Activity for easier reusage

--- library/src/main/java/io/multimoon/colorful/Colorful.kt
+++ library/src/main/java/io/multimoon/colorful/Colorful.kt
@@ -4,41 +4,45 @@ import android.app.Application
 import android.content.Context
 import android.util.Log
 
-var mInstance:ColorfulDelegate?=null
-val primaryThemeKey:String="primary_theme"
-val accentThemeKey:String="accent_theme"
-val darkThemeKey:String="dark_theme"
-val customThemeKey:String="custom_theme"
-val translucentKey:String="translucent"
+var mInstance: ColorfulDelegate? = null
+val primaryThemeKey: String = "primary_theme"
+val accentThemeKey: String = "accent_theme"
+val darkThemeKey: String = "dark_theme"
+val customThemeKey: String = "custom_theme"
+val translucentKey: String = "translucent"
 
-fun Colorful():ColorfulDelegate {
+fun Colorful(): ColorfulDelegate {
     mInstance?.let { return it }
     throw Exception("Colorful has not been initialized! Please call initColorful(app:Application) in your application class before working with Colorful!")
 }
 
-fun initColorful(app:Application, defaults:Defaults=Defaults(ThemeColor.INDIGO,ThemeColor.RED,true, true)) {
-    val time:Long=System.currentTimeMillis()
-    val prefs=app.getSharedPreferences("io.multimoon.colorful.colorvals", Context.MODE_PRIVATE)
+fun initColorful(app: Application, defaults: Defaults = Defaults(ThemeColor.INDIGO, ThemeColor.RED, true, true)) {
+    val time: Long = System.currentTimeMillis()
+    val prefs = app.getSharedPreferences("io.multimoon.colorful.colorvals", Context.MODE_PRIVATE)
 
-    mInstance=ColorfulDelegate(ThemeColor.valueOf(prefs.getString(primaryThemeKey,defaults.primaryColor.name)),
-            ThemeColor.valueOf(prefs.getString(accentThemeKey,defaults.accentColor.name)),
-            prefs.getBoolean(darkThemeKey,defaults.useDarkTheme), prefs.getBoolean(translucentKey, defaults.translucent), prefs.getInt(customThemeKey, defaults.customTheme))
+    mInstance = ColorfulDelegate(
+            ThemeColor.valueOf(prefs.getString(primaryThemeKey, defaults.primaryColor.name)),
+            ThemeColor.valueOf(prefs.getString(accentThemeKey, defaults.accentColor.name)),
+            prefs.getBoolean(darkThemeKey, defaults.useDarkTheme),
+            prefs.getBoolean(translucentKey, defaults.translucent),
+            prefs.getInt(customThemeKey, defaults.customTheme))
 
-    Log.d("COLORFUL", "Colorful init in " + (System.currentTimeMillis()-time) + " milliseconds!")
+    Log.d("COLORFUL", "Colorful init in " + (System.currentTimeMillis() - time) + " milliseconds!")
 }
 
-internal fun applyEdits(context:Context, primaryColor:ThemeColor, accentColor:ThemeColor, darkTheme:Boolean, translucent:Boolean, customTheme:Int=0) {
-    val prefs=context.getSharedPreferences("io.multimoon.colorful.colorvals", Context.MODE_PRIVATE)
+internal fun applyEdits(context: Context, primaryColor: ThemeColor, accentColor: ThemeColor, darkTheme: Boolean, translucent: Boolean, customTheme: Int = 0) {
+    val prefs = context.getSharedPreferences("io.multimoon.colorful.colorvals", Context.MODE_PRIVATE)
     prefs.edit()
             .putBoolean(darkThemeKey, darkTheme)
             .putString(primaryThemeKey, primaryColor.name)
             .putString(accentThemeKey, accentColor.name)
             .putBoolean(translucentKey, translucent)
             .apply()
-    if (customTheme!=0) prefs.edit().putInt(customThemeKey, customTheme).apply()
-    mInstance=ColorfulDelegate(primaryColor, accentColor, darkTheme, translucent)
+    if (customTheme != 0)
+        prefs.edit().putInt(customThemeKey, customTheme).apply()
+    mInstance = ColorfulDelegate(primaryColor, accentColor, darkTheme, translucent)
 }
 
-internal fun resetPrefs(context:Context) {
+internal fun resetPrefs(context: Context) {
     context.getSharedPreferences("io.multimoon.colorful.colorvals", Context.MODE_PRIVATE).edit().clear().apply()
 }
--- library/src/main/java/io/multimoon/colorful/ColorfulDelegate.kt
+++ library/src/main/java/io/multimoon/colorful/ColorfulDelegate.kt
@@ -5,34 +5,34 @@ import android.content.Context
 import android.support.annotation.StyleRes
 import android.util.Log
 
-class ColorfulDelegate(private var primaryColor:ThemeColor,private var accentColor:ThemeColor,private var darkTheme:Boolean,private var translucent:Boolean,private @StyleRes val customTheme:Int=0) {
+class ColorfulDelegate(private var primaryColor: ThemeColor, private var accentColor: ThemeColor, private var darkTheme: Boolean, private var translucent: Boolean, private @StyleRes val customTheme: Int = 0) {
 
-    fun apply(activity:Activity, override:Boolean = true, appcompat:Boolean = false) {
+    fun apply(activity: Activity, override: Boolean = true, appcompat: Boolean = false) {
         if (appcompat) {
             if (override) activity.setTheme(if (darkTheme) R.style.Colorful_AppCompat_Dark else R.style.Colorful_AppCompat_Light)
-            Log.d("COLORFUL","Using appcompat theme over native material per user preference")
+            Log.d("COLORFUL", "Using appcompat theme over native material per user preference")
         } else {
             if (override) activity.setTheme(if (darkTheme) R.style.Colorful_Dark else R.style.Colorful_Light)
         }
         activity.theme.applyStyle(primaryColor.primaryStyle(), true)
         activity.theme.applyStyle(accentColor.accentStyle(), true)
-        if (customTheme!=0) {
+        if (customTheme != 0) {
             activity.theme.applyStyle(customTheme, true)
             Log.d("COLORFUL", "Overriding native colorful theme with user provided theme at StyleRes $customTheme")
         }
     }
 
-    fun getPrimaryColor():ThemeColor=primaryColor
+    fun getPrimaryColor(): ThemeColor = primaryColor
 
-    fun getAccentColor():ThemeColor=accentColor
+    fun getAccentColor(): ThemeColor = accentColor
 
-    fun getDarkTheme():Boolean=darkTheme
+    fun getDarkTheme(): Boolean = darkTheme
 
-    fun getTranslucent():Boolean=translucent
+    fun getTranslucent(): Boolean = translucent
 
-    fun getCustomTheme():Int=customTheme
+    fun getCustomTheme(): Int = customTheme
 
-    fun edit():ThemeEditor {
+    fun edit(): ThemeEditor {
         return ThemeEditor(primaryColor, accentColor, darkTheme, translucent, customTheme)
     }
 
@@ -40,8 +40,8 @@ class ColorfulDelegate(private var primaryColor:ThemeColor,private var accentCol
         resetPrefs(context)
     }
 
-    internal fun getThemeString():String {
-        return primaryColor.name+":"+accentColor.name+":"+darkTheme+":"+customTheme+":"+translucent
+    internal fun getThemeString(): String {
+        return primaryColor.name + ":" + accentColor.name + ":" + darkTheme + ":" + customTheme + ":" + translucent
     }
 
 }
