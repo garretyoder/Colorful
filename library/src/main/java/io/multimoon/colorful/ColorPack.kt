a0b0cf965e7231cfc6c3ee10bf71c4db37ec62d6
polaric <multithedev@gmail.com>
2018-04-12 13:23:18 -0400

    Rename ColorPack.light to something that makes more sense

--- library/src/main/java/io/multimoon/colorful/ColorPack.kt
+++ library/src/main/java/io/multimoon/colorful/ColorPack.kt
@@ -1,6 +1,6 @@
 package io.multimoon.colorful
 
 class ColorPack(internal val colorLight: ColorfulColor, internal val colorDark: ColorfulColor) {
-    fun light() = colorLight
+    fun normal() = colorLight
     fun dark() = colorDark
 }
