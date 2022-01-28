94b22ba32e86858f71ca36d556db4a5b0b544a09
AyagiKei <ayagikei@163.com>
2022-01-29 01:11:30 +0800

    feat: add custom material you theme param

--- library/src/main/java/io/multimoon/colorful/ColorfulDelegate.kt
+++ library/src/main/java/io/multimoon/colorful/ColorfulDelegate.kt
@@ -12,10 +12,12 @@ class ColorfulDelegate(
     private var accentColor: ThemeColorInterface,
     private var darkTheme: Boolean,
     private var translucent: Boolean,
-    @StyleRes private val customTheme: Int = 0
+    @StyleRes private val customTheme: Int = 0,
+    @StyleRes private val customMaterialYouTheme: Int = 0
 ) {
 
-    @SuppressLint("NewApi") fun apply(
+    @SuppressLint("NewApi")
+    fun apply(
         activity: Activity,
         override: Boolean = true,
         baseTheme: BaseTheme = BaseTheme.THEME_APPCOMPAT
@@ -24,7 +26,13 @@ class ColorfulDelegate(
             when (baseTheme) {
                 BaseTheme.THEME_MATERIAL -> activity.setTheme(if (darkTheme) R.style.Colorful_Dark else R.style.Colorful_Light)
                 BaseTheme.THEME_APPCOMPAT -> activity.setTheme(if (darkTheme) R.style.Colorful_AppCompat_Dark else R.style.Colorful_AppCompat_Light)
-                THEME_MATERIAL_YOU -> activity.setTheme(if (darkTheme) R.style.Colorful_MaterialYou_Dark else R.style.Colorful_MaterialYou_Light)
+                THEME_MATERIAL_YOU -> activity.setTheme(
+                    if (customMaterialYouTheme != 0) {
+                        customMaterialYouTheme
+                    } else {
+                        if (darkTheme) R.style.Colorful_MaterialYou_Dark else R.style.Colorful_MaterialYou_Light
+                    }
+                )
             }
         }
 
@@ -32,7 +40,10 @@ class ColorfulDelegate(
         activity.theme.applyStyle(accentColor.accentStyle(), true)
         if (customTheme != 0) {
             activity.theme.applyStyle(customTheme, true)
-            Log.d("COLORFUL", "Overriding native colorful theme with user provided theme at StyleRes $customTheme")
+            Log.d(
+                "COLORFUL",
+                "Overriding native colorful theme with user provided theme at StyleRes $customTheme"
+            )
         }
     }
 
